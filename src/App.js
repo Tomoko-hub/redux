0e9b1ae55ff1a7a2ed50fb2def86607a0472303d
Tomoko <haisaitomoco@gmail.com>
2021-10-15 10:28:42 +0300

    counter ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
+function App () {
+    
+  const [ count, setCount ] = useState(0)
+
+  const plus = () => {
+    setCount(count +1)
+  }
+
+  const minus = () => {
+    setCount( count -1)
+  }
+
+  return(
+    <div>
+      <h4>Count: { count } </h4>
+      <button onClick={(plus)}> + </button>
+      <button onClick={(minus)}> - </button>
     </div>
-  );
+  )
+
 }
 
 export default App;
